518276e04b7d00abe88db19bc0bc5bd9d4917a4c
Ted Chandler <trc202@gmail.com>
2013-02-14 00:10:28 -0600

    updated confirmation.php to better suit our needs
    Changed variable names to be more discriptive
    Fixed formatting
    Made comments more discriptive

--- confirmation.php
+++ confirmation.php
@@ -1,46 +1,66 @@
 <?php
 
-include('config.php');
 
-// Passkey that got from link
-$passkey=$_GET['passkey'];
+//--------------- Start sql setup -----------------------------------//
+//TODO: replace with single config file
+$host = "localhost"; // Host name
+$username = "root"; // Mysql username
+$password = "not telling you github"; // Mysql password
+$db_name = "discourseanalysis"; // Database name
+$dbLink = new mysqli("$host", "$username", "$password", "$db_name");
 
-$tbl_name1="temp_members_db";
+//Connect to the database
+if ($dbLink->connect_errno) {
+    printf("Connect failed: %s\n", $dbLink->connect_error);
+    exit();
+}
+//--------------------------End sql setup ---------------------------//
 
-// Retrieve data from table where row that match this passkey
-$sql1="SELECT * FROM $tbl_name1 WHERE confirm_code ='$passkey'";
-$result1=mysql_query($sql1);
+// temporary table name
+$tempTable="tempusersinfo";
+
+//permanent table name
+$permanentTable="usersinfo";
+
+// registration identifer for a user
+$registrationId=$_GET['registrationId'];
+
+
+// Retrieve data from table where row that match the unique id
+$uniqueIdsQuery="SELECT * FROM $tempTable WHERE confirm_code ='$registrationId'";
+$uniqueIdsQueryResult=$dbLink->query($uniqueIdsQuery);
 
 // If successfully queried
-if($result1){
-		// Count how many row has this passkey
-		$count=mysql_num_rows($result1);
+if($uniqueIdsQueryResult){
+	// Count how many row has this registrationId
+	$count = $uniqueIdsQueryResult->num_rows;
 
-		// if found this passkey in our database, retrieve data from table "temp_members_db"
+	// if found this registrationId in our database, retrieve data from table listed in 
 	if($count==1){
-		$rows=mysql_fetch_array($result1);
-		$name=$rows['name'];
-		$email=$rows['email'];
-		$password=$rows['password'];
-		$country=$rows['country'];
+	
+		$rows=$uniqueIdsQueryResult->fetch_array(MYSQLI_ASSOC);
+		$username=$rows['Username'];
+		$email=$rows['Email'];
+		$password=$rows['Password'];
+		$name=$rows['Name'];
 
-		$tbl_name2="registered_members";
+		// Copy data from the temp table into the permanent table
+		$moveDataQuery="INSERT INTO $permanentTable(Username, Password, Email, Name)VALUES('$username', '$password', '$email', '$name' )";
+		$successfullyActivated=$dbLink->query($moveDataQuery);
+		
+		// if successfully moved data from the temp table to the permanent table
+		// and display activation notice while deleting the old data from the temp table
+		if($successfullyActivated){
+			echo "Your account has been activated";
 
-		// Insert data that retrieves from "temp_members_db" into table "registered_members"
-		$sql2="INSERT INTO $tbl_name2(name, email, password, country)VALUES('$name', '$email', '$password', '$country')";
-		$result2=mysql_query($sql2);
-	} // if not found passkey, display message "Wrong Confirmation code"
+			// Delete information of this user from table "temp_members_db" that has this registrationId
+			$sql3="DELETE FROM $tempTable WHERE confirm_code = '$registrationId'";
+			$result3=$dbLink->query($sql3);
+		}
+		
+	} // if not found registrationId, display message "Wrong Confirmation code"
 	else{
-		echo "Wrong Confirmation code";
-	}
-
-	// if successfully moved data from table"temp_members_db" to table "registered_members" displays message "Your account has been activated" and don't forget to delete confirmation code from table "temp_members_db"
-	if($result2){
-		echo "Your account has been activated";
-
-		// Delete information of this user from table "temp_members_db" that has this passkey
-		$sql3="DELETE FROM $tbl_name1 WHERE confirm_code = '$passkey'";
-		$result3=mysql_query($sql3);
+		echo "Invalid Confirmation code";
 	}
 }
 ?>
