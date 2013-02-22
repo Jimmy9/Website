9e5c0c37b3004b64646911969d8521e1396a7d79
Ted Chandler <trc202@gmail.com>
2013-02-21 20:25:22 -0600

    Implemented Database and User Modules.
    Implemented Database module and user module, additionally added a
    library folder which holds the password hashing class.

--- DatabaseModule.php
+++ DatabaseModule.php
@@ -9,7 +9,7 @@ class DatabaseModule
 	var $username = "";
 	var $password = "";
 	var $db_name = "";
-	var $currLink = new mysqli();
+	var $currLink;
 	/**
 	 * This loads the default MySQL login
 	 * into the relevent variables.
@@ -20,7 +20,7 @@ class DatabaseModule
 		$this->username = "root";
 		$this->password = "not telling you github";
 		$this->db_name = "discourseanalysis";
-		
+		$this->currLink = new mysqli();
 	}
 	
 	/**
@@ -28,13 +28,28 @@ class DatabaseModule
 	 **/
 	function Connect()
 	{
-		return $this->Connect("$this->host", "$this->username", "$this->password", "$this->db_name");
+		$result = $this->currLink->Connect($this->host, $this->username, $this->password, $this->db_name);
+		
+		if(!$this->currLink->connect_errno) 
+		{
+			return $this->currLink;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 	/**
 	 * Connects to the MySQL server using custom data
+	 *
+	 * Note, this name is not Connect as overloading in php is not the same as other languages.
+	 * @param string $host the hostname of the database to login to.
+	 * @param string $username the username of the database to login to.
+	 * @param string $password the database of the database to login to.
+	 * @param string $db_name the database name of the database to login to.
 	 **/
-	function Connect($host, $username, $password, $db_name)
+	function ConnectUnique($host, $username, $password, $db_name)
 	{
 		$dbLink = new mysqli("$host", "$username", "$password", "$db_name");
 		
--- UserModule.php
+++ UserModule.php
@@ -1,44 +1,93 @@
 <?php
 
 /**
- * UserModule is a tool tht allows
+ * UserModule is a tool that allows
  * the page to login users in and out,
  * and check if the user is logged in.
  *
  **/
+ 
+require('./lib/PasswordHash.php');
+ 
 class UserModule
 {
 	var $dbConnect;
+	
 	/**
-	 * 
+	 * Default constructor, requires a active mysqli connection.
 	 **/
 	function UserModule($dbConnect)
 	{
-		
+		$this->dbConnect = $dbConnect;
+		if(session_id() == '')
+		{
+			session_start();
+		}
 	}
 	
 	/**
-	 *
+	 * Checks to see if the user is logged in. returns a boolean indicating the current status
 	 **/
-	function IsUserloggedIn()
+	function IsUserLoggedIn()
 	{
-	
+		return(isset($_SESSION['username']));
 	}
 	
 	/**
+	 * Logs in a user. Returns boolean indicating the result.
 	 *
+	 * @param string $userName Username of the person logging in.
+	 * @param string $password Password in plain text of the person logging in.
 	 **/
 	function LoginUser($userName, $password)
 	{
-	
+		if($stmt = $this->dbConnect->prepare("SELECT Password FROM usersinfo WHERE username=?"))
+		{
+			$stmt->bind_param("s", $userName);
+			$stmt->execute();
+			$stmt->bind_result($hashedPassword);
+			$stmt->fetch();
+			$stmt->close();
+			echo $hashedPassword;
+			$pwdHasher = new PasswordHash(8, FALSE);
+			if(!empty($hashedPassword) &&  $pwdHasher->CheckPassword($password, $hash))
+			{
+				$_SESSION['username'] = $userName;
+				return true;
+			}
+			//DEBUG LINE echo "<p> password or username is incorrect but everything is working!</p>";
+		}
+		return false;
 	}
 	
 	/**
+	 * Logs out a user returning a boolean indicating the result.
 	 *
+	 * Can only return false if the user was not logged in before calling this method.
+	 **/
+	function LogoutUser()
+	{
+		$loggedOut = true;
+		if(isset($_SESSION['username']))
+		{
+			unset ($_SESSION['username']);
+		}
+		else
+		{
+			$loggedOut = false;
+		}
+		return $loggedOut;
+	}
+	
+	/**
+	 * Returns a users username or an empty string if the user is not logged in.
 	 **/
 	function GetUserName()
 	{
-	
+		if(isset($_SESSION['username']))
+			return $_SESSION['username'];
+		else
+			return "";
 	}
 	
 	
