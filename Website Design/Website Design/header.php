84db7b0ea72649ffdea14e5776a1d4006bf6515d
Justin Ervin <JustinAErvin@gmail.com>
2013-02-24 23:05:03 -0600

    created header for website

---
+++ Website Design/Website Design/header.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+<html>
+<head>
+    <title>Discourse Analysis - Welcome!</title>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css" />
+
+</head>
+<body>
+    <div id="header">
+        <img src="logo.jpg" height="200" width="70%" alt="Logo" />
+        <div id="menu">
+            <ul id="navigation">
+                <li><a class="navButton" href="home.html">Home</a></li>
+                <li><a class="navButton" href="upload.html">Upload to Workspace</a></li>
+                <li><a class="navButton" href="myfiles.html">My Files</a></li>
+                <li>
+                    <a class="navButton" href="">Administrative Options</a>
+                    <ul>
+                        <li><a class="navButton" href="">option 1</a></li>
+                        <li><a class="navButton" href="">option 2</a></li>
+                        <li><a class="navButton" href="">option 3</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="navButton" href="">Login / Register</a>
+                    <ul>
+                        <li><a class="navButton" href="">Login</a></li>
+                        <li><a class="navButton" href="">Register</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="triangle-l"></div>
+        <div class="triangle-r"></div>
+    </div>
