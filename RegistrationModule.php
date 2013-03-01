365b1c225dd008f6530efa644789eba3c8488e99
Ted Chandler <trc202@gmail.com>
2013-03-01 10:54:01 -0600

    Filled in Registration Module
    Filled in Registration Module and created a registration test. Still
    need to hash the password when registering though.

--- RegistrationModule.php
+++ RegistrationModule.php
@@ -11,8 +11,6 @@ class RegistrationModule
 	private $password = "";
 	private $email = "";
 	private $realName = "";
-	private $tempUserTable = "";
-	private $userTable = "";
 	
 	/**
 	 *
@@ -20,8 +18,6 @@ class RegistrationModule
 	function RegistrationModule($dbConnect)
 	{
 		$this->dbConnect = $dbConnect;
-		$this->tempUserTable = "tempusersinfo"
-		$this->userTable = "userTable";
 	}
 	
 	/**
@@ -46,13 +42,21 @@ class RegistrationModule
 	{
 		if(!empty($username) && (strlen($username) >= 3))
 		{
-			//TODO check to see if the username exists in the database
-			return true;
+			if($stmt = $this->dbConnect->prepare("SELECT COUNT(Username) AS UsernameCount FROM ((SELECT tempusersinfo.Username FROM tempusersinfo) UNION (SELECT usersinfo.Username FROM usersinfo)) AS usernamestable WHERE Username = ?"))
+			{
+				$stmt->bind_param("s", $username);
+				$stmt->execute();
+				$stmt->bind_result($usernameCount);
+				$stmt->fetch();
+				$stmt->close();
+				if($usernameCount == 0)
+				{
+					$this->username = $username;
+					return true;
+				}	
+			}
 		}
-		else
-		{
-			return false;
-		}
+		return false;
 	}
 	
 	/**
@@ -60,8 +64,13 @@ class RegistrationModule
 	 **/
 	function InputPassword($password, $confirmPassword)
 	{
-		if(
-		
+		if((strcmp($password ,$confirmPassword) == 0) && (strlen($password) >= 6))
+		{
+			//TODO hash password here
+			$this->password = $password;
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -69,7 +78,12 @@ class RegistrationModule
 	 **/
 	function InputEmail($email, $confirmEmail)
 	{
-		
+		if((strcmp($email ,$confirmEmail) == 0) && filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$this->email = $email;
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -77,7 +91,39 @@ class RegistrationModule
 	 **/
 	function RequestConfirmation()
 	{
-		
+		if(strlen($this->username) > 0 && strlen($this->password) > 0 && strlen($this->email) > 0 && strlen($this->realName) > 0)
+		{
+			$confirm_code=md5(uniqid(rand()));
+			if($stmt = $this->dbConnect->prepare("INSERT INTO tempusersinfo(confirm_code, Username, Password, Email, Name)VALUES( ?, ?, ?, ?, ?)"))
+				{
+					$stmt->bind_param("sssss", $confirm_code, $this->username, $this->password, $this->email, $this->realName);
+					$stmt->execute();
+					$stmt->bind_result($result);
+					$stmt->fetch();
+					$stmt->close();
+					if($result)
+					{
+						$to=$this->email;
+
+						// Your subject
+						$subject="Your confirmation link here";
+
+						// From
+						$header="from: your name <your email>";
+
+						// Your message
+						$message="Your Comfirmation link \r\n";
+						$message.="Click on this link to activate your account \r\n";
+						$message.="http://trc202.com/confirmation.php?registrationId=$confirm_code";
+
+
+						// send email
+						$sentmail = mail($to,$subject,$message,$header);
+						return true;
+					}	
+				}
+		}
+		return false;
 	}
 	
 	
@@ -86,53 +132,32 @@ class RegistrationModule
 	 **/
 	function ConfirmUser($key)
 	{
-		if($stmt = $this->dbConnect->prepare("SELECT * FROM ? WHERE confirm_code=?"))
+		if($stmt = $this->dbConnect->prepare("SELECT Username, Email, Password, Name FROM tempusersinfo WHERE confirm_code= ? "))
 		{
-			$stmt->bind_param("ss", $this->tempUserTable, $key);
+			$stmt->bind_param("s", $key);
 			$stmt->execute();
-			$stmt->bind_result($keyResult);
+			$stmt->bind_result($username, $email, $password, $name);
 			$stmt->fetch();
-			$stmt->close();
-			if($uniqueIdsQueryResult)
+			$stmt->close();		
+			if($stmt = $this->dbConnect->prepare("INSERT INTO usersinfo(Username, Password, Email, Name)VALUES(?, ?, ?, ?)"))
 			{
-				// Count how many row has this registrationId
-				$count = $uniqueIdsQueryResult->num_rows;
-
-				// if found this registrationId in our database, retrieve data from table listed in
-				if($count==1)
+				$stmt->bind_param("ssss", $username, $password, $email, $name);
+				$stmt->execute();
+				$stmt->bind_result();
+				$stmt->fetch();
+				$stmt->close();
+				if($stmt = $this->dbConnect->prepare("DELETE FROM tempusersinfo WHERE confirm_code = ?"))
 				{
-					$rows=$uniqueIdsQueryResult->fetch_array(MYSQLI_ASSOC);
-					$username=$rows['Username'];
-					$email=$rows['Email'];
-					$password=$rows['Password'];
-					$name=$rows['Name'];
-					
-					if($stmt = $this->dbConnect->prepare("INSERT INTO ?(Username, Password, Email, Name)VALUES(?, ?, ?, ?)"))
-					{
-						$stmt->bind_param("sssss", $this->userTable, $username, $password, $email, $name);
-						$stmt->execute();
-						$stmt->bind_result($successfullyActivated);
-						$stmt->fetch();
-						$stmt->close();
-						if($successfullyActivated){
-							if($stmt = $this->dbConnect->prepare("DELETE FROM ? WHERE confirm_code = ?"))
-							{
-								$stmt->bind_param("ss", $this->tempUserTable, $key);
-								$stmt->execute();
-								$stmt->bind_result($removed);
-								$stmt->fetch();
-								$stmt->close();
-							}
-							return true;
-						}
-					}
+					$stmt->bind_param("s", $key);
+					$stmt->execute();
+					$stmt->bind_result($removed);
+					$stmt->fetch();
+					$stmt->close();
 				}
+				return true;
 			}
+			return false;
 		}
-		return false;
-		
 	}
-	
 }
-
 ?>
---
+++ testRegistration.php
@@ -0,0 +1,86 @@
+<?php
+require('DatabaseModule.php');
+require('RegistrationModule.php');
+
+//START DB TEST
+$dbMod = new DatabaseModule();
+$connection = $dbMod->connect();
+if($connection == false){
+	echo "<p> Not Connected to the database </p>";
+}else{
+	echo "Connected to the database";
+}
+//END DB TEST
+
+//START RegistrationModule TEST
+echo "<p>Creating registration Module</p>";
+$regMod = new RegistrationModule($connection);
+echo "<p>Registration Module Created</p>";
+echo "<p>Testing Input Name</p>";
+if(!($regMod->InputName("",""))){
+	echo "<p>1/4 Input Name test success</p>";
+}
+if(!($regMod->InputName("First",""))){
+	echo "<p>2/4 Input Name test success</p>";
+}
+if(!($regMod->InputName("","Last"))){
+	echo "<p>3/4 Input Name test success</p>";
+}
+if($regMod->InputName("First","Last")){
+	echo "<p>4/4 Input Name test success</p>";
+}
+echo "<p>Finished testing Input Name, Make sure all 4 tests passed</p>";
+echo "<p></p>";
+
+
+
+echo "<p>Testing Input Username</p>";
+if(!($regMod->InputUsername(""))){
+	echo "<p>1/3 Null username test sucess</p>";
+}
+if(!($regMod->InputUsername("trc202"))){
+	echo "<p>2/3 existing user test passed</p>";
+}
+if(($regMod->InputUsername("newUser"))){
+	echo "<p>3/3 new user test passed</p>";
+}
+echo "<p>Finished testing Input Username, Make sure all 3 tests passed</p>";
+echo "<p></p>";
+
+echo "<p>Testing Input Password</p>";
+if(!($regMod->InputPassword("", ""))){
+	echo "<p>1/3 Null password test sucess</p>";
+}
+if(!($regMod->InputPassword("different", "passwords"))){
+	echo "<p>2/3  password test sucess</p>";
+}
+if(($regMod->InputPassword("thisisworkingtest", "thisisworkingtest"))){
+	echo "<p>3/3  password test sucess</p>";
+}
+echo "<p>Finished testing password</p>";
+echo "<p></p>";
+echo "<p>Testing email input</p>";
+if(!($regMod->InputEmail("",""))){
+	echo "<p>1/3 email test sucess</p>";
+}
+if(!($regMod->InputEmail("test","1234"))){
+	echo "<p>2/3 email test sucess</p>";
+}
+if(($regMod->InputEmail("trc202@gmail.com","trc202@gmail.com"))){
+	echo "<p>3/3 email test sucess</p>";
+}
+echo "<p>Finished email input</p>";
+echo "<p></p>";
+
+echo "Testing send confirmation";
+if($regMod->RequestConfirmation()){
+	echo "<p> Sending confirmation successfull </p>";
+}
+echo "<p>Finished testing send confirmation</p>";
+echo "<p></p>";
+
+echo "Testing ConfirmUser";
+if($regMod->ConfirmUser("5eb8c8e9116f5c34d245ad60bfaedcf8")){
+	echo "<p> Confirm User successfull </p>";
+}
+echo "Finished Testing ConfirmUser";
