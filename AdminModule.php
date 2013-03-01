5c77f85066c75fd1383b33d331c79b127bf1c84e
Justin R. Hurst <Jryanhurst@gmail.com>
2013-03-01 11:12:27 -0600

    Created AdminModules and added functions

---
+++ AdminModule.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This module handles the commands the admin
+ * will be able to use to control users and 
+ * their files. 
+ *
+ * All of these functions will check 
+ * admin creditentials before proceeding
+ *
+ **/
+
+ 
+ 
+
+class AdminUserModule
+{
+	function ChangeUsername()
+	{
+	
+	}
+	
+	function ChangePassword()
+	{
+	
+	}
+	
+	function ChangeEmail()
+	{
+	
+	}
+	
+	function ChangeName()
+	{
+	
+	}
+	
+	function DeleteUser()
+	{
+	
+	}
+	
+	function ViewUser()
+	{
+	
+	}
+	
+	function QueryUserList()
+	{
+	
+	}
+} 
+class AdminFileModule
+{
+	function EditFileOwner()
+	{
+	
+	}
+	
+	function ChangeFilePermissions()
+	{
+	
+	}
+	
+	function EditFilename()
+	{
+	
+	}
+	
+	function EditFileLocation()
+	{
+	
+	}
+	
+	Function DeleteFile()
+	{
+	
+	}
+	
+	Function ViewFile()
+	{
+	
+	}
+	
+	Function GetUserFileList()
+	{
+	
+	}
+	
+	Function AddFileToUser()
+	{
+	
+	}
+	
+	
+}
+
+?>
