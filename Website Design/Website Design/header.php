b7a94f1078f5da7bc8870d11f3dd3fbe2303a272
Justin Ervin <JustinAErvin@gmail.com>
2013-03-13 12:30:17 -0500

    added jQuery external link to header

--- Website Design/Website Design/header.php
+++ Website Design/Website Design/header.php
@@ -6,6 +6,7 @@
 <head>
     <title>Discourse Analysis - Welcome!</title>
     <link rel="stylesheet" href="stylesheet.css" type="text/css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 </head>
 <body>
